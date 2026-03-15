955902d39a0b5e5af2b0aa5ee6465d6cbdc29a05
kolmipilot <92938734+kolmipilot@users.noreply.github.com>
2026-03-15 18:03:28 +0100

    ready melb camera

---
+++ addons/compat_melb/script_component.hpp
@@ -0,0 +1,18 @@
+#define COMPONENT compat_melb
+#define COMPONENT_BEAUTIFIED Compat_melb
+#include "\z\ibc\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define CBA_DEBUG_SYNCHRONOUS
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_COMPAT_MELB
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_COMPAT_MELB
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_COMPAT_MELB
+#endif
+
+#include "\z\ibc\addons\main\script_macros.hpp"
